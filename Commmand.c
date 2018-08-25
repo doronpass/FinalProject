396cc61cc37162c7ac957125ccfdd787ca7c485f
doron <doronpassal@gmail.com>
2018-08-25 14:23:38 +0300

    func creat_new_command added

--- Commmand.c
+++ Commmand.c
@@ -3,3 +3,12 @@
 //
 
 #include "Commmand.h"
+
+*Command create_new_command(enum command_name name ,int *args_arr )/* to check I took the enum value in the right way */
+{
+    Command *new_command =(*Command)malloc(sizeof(Command));
+    new_command->name = name;
+    new_command->arg_arr = args_arr;
+    new_command->num_of_args = int(name);
+    return new_command;
+}
--- Commmand.h
+++ Commmand.h
@@ -4,9 +4,27 @@
 
 #ifndef FINALPROJECT_COMMMAND_H
 #define FINALPROJECT_COMMMAND_H
+enum command_name {
+    solve = 1 ,
+    edit = 1,
+    mark_errors = 1 ,
+    print_board = 0,
+    set = 3,
+    validate = 0 ,
+    generate = 2,
+    undo = 0,
+    redo = 0,
+    save = 1,
+    hint = 2,
+    num_solutions = 0,
+    autofill = 0,
+    reset = 0,
+    exit = 0,
+        };
+
 typedef struct
 {
-    enum command_name;
+    enum command_name name;
     int arg_arr[3]; // to make sure it with zeroes
     int num_of_args;
     //to enter var to make arg
