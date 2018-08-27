83ad205eda327b44ce10d6ba470da61203efbadd
doron <doronpassal@gmail.com>
2018-08-27 13:26:19 +0300

    nothing important

--- Game.c
+++ Game.c
@@ -9,7 +9,7 @@
 Game *create_new_game (Cell **user_game_board,Cell **solved_game_board,int mark_error,int mode,int m_block_rows, int n_block_cols,Doubly_linked_list *dll){
     Game *new_game = (Game*)malloc(sizeof(Game));
     if (new_game == NULL){
-        printf("Error: calloc has failed\n");
+        printf("Error: malloc has failed\n");
         exit(0);
     }
     new_game->n_block_cols = n_block_cols;
