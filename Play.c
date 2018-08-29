1fb12ec7769acb092498f6cfc2d9f1417e021a41
doron <doronpassal@gmail.com>
2018-08-29 12:28:18 +0300

    before merge

--- Play.c
+++ Play.c
@@ -6,6 +6,38 @@
 #include "Play.h"
 #include "Game.h"
 
-void start_play(){
-    printf("Sudoku \n------\n");
+void do_solve (char *x){
+    create_new_command("SOLVE",)
 }
+
+void do_solve (char *x){}
+
+void do_edit(char *x){}
+
+void do_mark_errors (char *x){}
+
+void do_print_board (char *x){}
+
+void do_set (char *x){}
+
+void do_validate (char *x){}
+
+void do_generate (char *x){}
+
+void do_undo (char *x){}
+
+void do_redo (char *x){}
+
+void do_save (char *x){}
+
+void do_hint (char *x){}
+
+void do_num_soulutions (char *x){}
+
+void do_autofill (char *x){}
+
+void do_reset (char *x){}
+
+void do_exit (char *x){}
+
+void do_solve (char *x){}
