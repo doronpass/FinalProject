4674dfdcf5c1e5b30dcd16083994ad4ca0e5f7fd
doron <doronpassal@gmail.com>
2018-08-29 13:57:01 +0300

    delete command.c and command.h and move it to Dll

--- Dll.h
+++ Dll.h
@@ -4,14 +4,18 @@
 
 #ifndef FINALPROJECT_DLL_H
 #define FINALPROJECT_DLL_H
-#include "Command.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 typedef struct
 {
-    struct Node *first;
-    struct Node *last;
-    int doubly_linked_list_size;
-} Doubly_linked_list;
+    char* command_name;
+    int x;
+    int y;
+    int value;
+    int prev_value;
+    int autofill_counter;
+} Command;
 
 typedef struct Node {
     struct Node *next;
@@ -20,8 +24,19 @@ typedef struct Node {
 
 } Node;
 
+
+typedef struct
+{
+    struct Node *first;
+    struct Node *last;
+    int doubly_linked_list_size;
+} Doubly_linked_list;
+
+
 void add_last(Doubly_linked_list *dll, Command *node_data);
 Doubly_linked_list* create_new_dll();
 void remove_last (Doubly_linked_list *dll);
+Command* create_new_command(char* command_name ,int *args_arr ,int num_of_args);
+
 
 #endif // FINALPROJECT_DLL_H
