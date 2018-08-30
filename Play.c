92431d6f3eb5dd82e93f8c2a1d7ddd15f4fcbf01
doron <doronpassal@gmail.com>
2018-08-30 16:55:13 +0300

    undo funct accept genreate undo

--- Play.c
+++ Play.c
@@ -49,16 +49,30 @@ void do_undo (Game *game ,int x, int y , int value , int pre_value){ // i need t
         nothing_to_undo();
     }
     else {
-        if (strcmp(p->dll_pointer->node_data->command_name,"set")){
+        if (strcmp(p->dll_pointer->node_data->command_name,"set")){ /* if it's regular set command */
             p->dll_pointer = p->dll_pointer->prev;
             do_set(game,x,y,pre_value);
             print_user_board(game);
             undo_print(x,y,pre_value,value);
         }
         else{
-            if (strcmp(p->dll_pointer->node_data->command_name,"autofull")){
+            if (strcmp(p->dll_pointer->node_data->command_name,"autofull")){ /* if it's autofillcommand */
+                while(p->dll_pointer->node_data->autofill_counter != 0){ /* while op_number in the node is not 0 keep going back */
+                    do_set(game,p->dll_pointer->node_data->x,p->dll_pointer->node_data->y,p->dll_pointer->node_data->prev_value); /* each time undo the current step */
+                    p->dll_pointer = p->dll_pointer->prev; /* and go one back to keep checking */
+                }
+                do_set(game,p->dll_pointer->node_data->x,p->dll_pointer->node_data->y,p->dll_pointer->node_data->prev_value); /* also do the first to happen */
+                while ((p->dll_pointer->node_data->autofill_counter == 0) || (!strcmp(p->dll_pointer->node_data->command_name,"autofull"))){ /* now moving forward to do all prints required */
+                    undo_print(p->dll_pointer->node_data->x,p->dll_pointer->node_data->y,p->dll_pointer->node_data->prev_value,p->dll_pointer->node_data->value);
+                    p->dll_pointer = p->dll_pointer->next;
+                }
+                while(p->dll_pointer->node_data->autofill_counter != 0){       /* while op_number in the node is not 0 keep going back */
+                    p->dll_pointer = p->dll_pointer->prev;
+                }
+                p->dll_pointer = p->dll_pointer->prev;
+                print_user_board(game); /* print board at the end of autofull undo*/
             }
-            else if (strcmp(p->dll_pointer->node_data->command_name,"generate")){
+            else if (strcmp(p->dll_pointer->node_data->command_name,"generate")){ /* stiil to do ********************/
             }
          }
     }
