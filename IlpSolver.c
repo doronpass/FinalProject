1893527fa78f35a9f7d1e8af176834353a4ef3a0
doron <doronpassal@gmail.com>
2018-09-07 13:21:54 +0300

    the gurobi almost finishd just two function left

--- IlpSolver.c
+++ IlpSolver.c
@@ -8,6 +8,23 @@
 #include "Game.h"
 
 
+/* free all the other stuff we used */     /* Free environment */     /* Free model */
+void free_grb(int *ind ,int *indarr2,double *val , double *valarr2, double *sol , char *vtype, GRBenv *env,
+              GRBmodel *model,double *lb ){
+
+    free(sol);
+    free(vtype);
+    free(lb);
+    free(val);
+    free(ind);
+    GRBfreemodel(model);
+    GRBfreeenv(env);
+    free(valarr2);
+    free(indarr2);
+}
+
+
+
 void copy_sol_to_board(int *sol, Game *game) {
     int N = (game->m_mult_n)*(game->m_mult_n);
     int i, j, v;
@@ -29,61 +46,66 @@ int ilp_solver(Game *game) {
     GRBmodel *model = NULL;
     char inputline[100];
     int *ind;
+    int *indarr2;
     double *val;
+    double *valarr2;
     double *lb;
     char *vtype;
-    char *names;
-    char *namestorage;
-    char *cursor;
     int optimstatus;
     double objval;
     int zero = 0;
     int i, j, v, ig, jg, count;
     int error = 0;
     int N;
-    int *sol;
+    int flag = 0;
+    double *sol;
     int temportry_value;
     /* initialize arrays and pointer for later use */
 
-    sol = (int *) malloc(N * N * N);
-    Cell **user_board = game->user_game_board;
+    sol = (double *) malloc(N * N * N * sizeof(double));
     N = game->m_mult_n * game->m_mult_n;
-    namestorage = (char *) malloc(10 * N * N * N);
     vtype = (char *) malloc(N * N * N);
-    lb = (double *) malloc(N * N * N);
-    val = (double *) malloc(N);
-    ind = (int *) malloc(N);
-    names = (char *) malloc(N * N * N);
+    lb = (double *) malloc(N * N * N* sizeof(double));
+    val = (double *) malloc(N * sizeof(double));
+    ind = (int *) malloc(N * sizeof(int));
+    valarr2 = (double *) malloc(N * sizeof(double));
+    indarr2 = (int *) malloc(N * sizeof(int));
 
     /* Create an empty model */
 
-    cursor = namestorage;
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
             for (v = 0; v < N; v++) {
-                if (user_board[i][j].value == v) /* to make sure it includes our value in the existiong board */
+                if (game->user_game_board[i][j].value == v+1) /* tto make sure that te plus 1 is needed */
                     lb[i * N * N + j * N + v] = 1;
                 else
                     lb[i * N * N + j * N + v] = 0;
                 vtype[i * N * N + j * N + v] = GRB_BINARY;
 
-                names[i * N * N + j * N + v] = cursor;
-                sprintf(names[i * N * N + j * N + v], "x[%d,%d,%d]", i, j, v + 1);
-                cursor += strlen(names[i * N * N + j * N + v]) + 1;
+
             }
-        }
-    }
+        }}
 
     /* Create environment */
 
     error = GRBloadenv(&env, "sudoku.log");
-    if (error) goto QUIT;
+    if (error) {
+        printf("Error: %s\n",GRBgeterrormsg(env)));
+        free_grb(ind ,indarr2,val ,valarr2, sol ,vtype,env,
+                 model,lb);
+        return flag;
+    }
 
     /* Create new model */
 
     error = GRBnewmodel(env, &model, "sudoku", N * N * N, NULL, lb, NULL,
-                        vtype, names);
-    if (error) goto QUIT;
+                        vtype, NULL);
+    if (error) {
+        printf("Error: %s\n",GRBgeterrormsg(env)));
+        free_grb(ind ,indarr2,val ,valarr2, sol ,vtype,env,
+                 model,lb);
+        return flag;
+    }
 
     /* Each cell gets a value */
 
@@ -95,28 +117,30 @@ int ilp_solver(Game *game) {
             }
 
             error = GRBaddconstr(model, N, ind, val, GRB_EQUAL, 1.0, NULL);
-            if (error) goto QUIT;
+            if (error) {
+                printf("Error: %s\n",GRBgeterrormsg(env)));
+                free_grb(ind ,indarr2,val ,valarr2, sol ,vtype,env,
+                         model,lb);
+                return flag;
+            }
         }
     }
-
-
-
+/* this is the problematic one, if there are problem to check here */
     /* firest consraint that we might sould dump assure that the values that come in boars stay in place */
 /* and sould check that in line 4 it realy user game board and not solved one */
     for (j = 0; j < N; j++) {
         for (i = 0; i < N; i++) {
             temportry_value = game->user_game_board[j][i].value;
             if (0 != temportry_value) {
-                for (v = 0; v < N; ++v) {
-                    if (temportry_value == v + 1) {
-                        ind[v] = i * N * N + i * N + v;
-                        val[v] = 1.0;
-
-                    }
+                valarr2[0] = 1;
+                indarr2[0] = N*N*j + i*N+(temportry_value - 1);
+                error = GRBaddconstr(model, 1, indarr2, valarr2, GRB_EQUAL, 1.0, NULL);
+                if (error) {
+                    printf("Error: %s\n",GRBgeterrormsg(env)));
+                    free_grb(ind ,indarr2,val ,valarr2, sol ,vtype,env,
+                             model,lb);
+                    return flag;
                 }
-                error = GRBaddconstr(model, size, ind, val, GRB_EQUAL, 1.0, NULL);
-                if (error) goto QUIT;
-
             }
         }
     }
@@ -131,8 +155,12 @@ int ilp_solver(Game *game) {
             }
 
             error = GRBaddconstr(model, N, ind, val, GRB_EQUAL, 1.0, NULL);
-            if (error) goto QUIT;
-        }
+            if (error) {
+                printf("Error: %s\n",GRBgeterrormsg(env)));
+                free_grb(ind ,indarr2,val ,valarr2, sol ,vtype,env,
+                         model,lb);
+                return flag;
+            }        }
     }
 
     /* Each value must appear once in each column */
@@ -145,8 +173,12 @@ int ilp_solver(Game *game) {
             }
 
             error = GRBaddconstr(model, N, ind, val, GRB_EQUAL, 1.0, NULL);
-            if (error) goto QUIT;
-        }
+            if (error) {
+                printf("Error: %s\n",GRBgeterrormsg(env)));
+                free_grb(ind ,indarr2,val ,valarr2, sol ,vtype,env,
+                         model,lb);
+                return flag;
+            }        }
     }
 
     /* Each value must appear once in each subgrid */
@@ -164,46 +196,66 @@ int ilp_solver(Game *game) {
                 }
 
                 error = GRBaddconstr(model, DIM, ind, val, GRB_EQUAL, 1.0, NULL);
-                if (error) goto QUIT;
-            }
+                if (error) {
+                    printf("Error: %s\n",GRBgeterrormsg(env)));
+                    free_grb(ind ,indarr2,val ,valarr2, sol ,vtype,env,
+                             model,lb);
+                    return flag;
+                }            }
         }
     }
 
     /* Optimize model */
 
     error = GRBoptimize(model);
-    if (error) goto QUIT;
-
+    if (error) {
+        printf("Error: %s\n",GRBgeterrormsg(env)));
+        free_grb(ind ,indarr2,val ,valarr2, sol ,vtype,env,
+                 model,lb);
+        return flag;
+    }
     /* Write model to 'sudoku.lp', int the tuturial they aid not necessary but very helpful */
 
     error = GRBwrite(model, "sudoku.lp");
-    if (error) goto QUIT;
-
+    if (error) {
+        printf("Error: %s\n",GRBgeterrormsg(env)));
+        free_grb(ind ,indarr2,val ,valarr2, sol ,vtype,env,
+                 model,lb);
+        return flag;
+    }
     /* Capture solution information */
 
     error = GRBgetintattr(model, GRB_INT_ATTR_STATUS,
                           &optimstatus); /*/query the status of the optimization process by retrieving the value of the Status attribute*/
-    if (error) goto QUIT;
-
+    if (error) {
+        printf("Error: %s\n",GRBgeterrormsg(env)));
+        free_grb(ind ,indarr2,val ,valarr2, sol ,vtype,env,
+                 model,lb);
+        return flag;
+    }
     /* this call would return a non-zero error result if no solution was found for this model.*/
 
     error = GRBgetdblattr(model, GRB_DBL_ATTR_OBJVAL,
                           &objval); /* the value of the objective function for the computed solution*/
-    if (error) goto QUIT;
-
+    if (error) {
+        printf("Error: %s\n",GRBgeterrormsg(env)));
+        free_grb(ind ,indarr2,val ,valarr2, sol ,vtype,env,
+                 model,lb);
+        return flag;
+    }
     /*this routine retrieves the values of an array-valued attribute. The third and fourth arguments indicate the index of the first array element to be retrieved, and the number of elements to retrieve*/
     error = GRBgetdblattrarray(model, GRB_DBL_ATTR_X, 0, N * N * N, sol); /* func i added from the example */
     if (error) {
-        printf("ERROR %d GRBgetdblattrarray(): %s\n", error, GRBgeterrormsg(env));
-        return -1;
+        printf("Error: %s\n",GRBgeterrormsg(env)));
+        free_grb(ind ,indarr2,val ,valarr2, sol ,vtype,env,
+                 model,lb);
+        return flag;
     }
-
-    printf("\nOptimization complete\n");
-
 
     /* the sol is a good one so we thake it to out solver board in game and make it be the same */
     if (optimstatus == GRB_OPTIMAL) {
         copy_to_sol_board(sol, game);
+        flag = 1 ;
         printf("Optimal objective: %.4e\n", objval);
     } else if (optimstatus == GRB_INF_OR_UNBD)
         printf("Model is infeasible or unbounded\n");
@@ -211,33 +263,17 @@ int ilp_solver(Game *game) {
         printf("Optimization was stopped early\n");
     printf("\n");
 
-    QUIT:
-
-    /* Error reporting */
 
     if (error) {
-        printf("ERROR: %s\n", GRBgeterrormsg(env));
-        exit(1);
+        printf("Error: %s\n",GRBgeterrormsg(env)));
+        free_grb(ind ,indarr2,val ,valarr2, sol ,vtype,env,
+                model,lb);
+        return flag;
     }
 
-    /* copy sol to user_board */
+    return  flag;
+
+    /* copy sol to game->user_game_board */
 
 
-
-
-
-
-    void free_ilp() {/* free all the other stuff we used */     /* Free environment */     /* Free model */
-
-
-        free(sol);
-        free(user_board);
-        free(namestorage);
-        free(vtype);
-        free(lb);
-        free(val);
-        free(ind);
-        GRBfreemodel(model);
-        GRBfreeenv(env);
-    }
 }
--- IlpSolver.h
+++ IlpSolver.h
@@ -5,4 +5,12 @@
 #ifndef FINALPROJECT_ILPSOLVER_H
 #define FINALPROJECT_ILPSOLVER_H
 
+#include "Game.h"
+
+void free_ilp();
+int ilp_solver(Game *game) ;
+void copy_sol_to_board(int *sol, Game *game) ;
+void free_grb(int *ind ,int *indarr2,double *val , double *valarr2, double *sol , char *vtype, GRBenv *env,
+              GRBmodel *model,double *lb );
+
 #endif //FINALPROJECT_ILPSOLVER_H
