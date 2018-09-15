09650fc0a426b4f79e357733015ff0a240c06a1d
itaizur <itaizur@mail.tau.ac.il>
2018-09-15 12:39:15 +0300

    test if working

--- Game_board.c
+++ Game_board.c
@@ -153,7 +153,6 @@ int** create_matrix(int N){
     }
     return arr;
 }
-
 void copy_board_to_game(int **result_arr,int N,Game *game){
     int i,j;
 
@@ -166,7 +165,7 @@ void copy_board_to_game(int **result_arr,int N,Game *game){
         }
     }
 }
-void copy_sol_to_board(double *sol,int **result_arr, int N ) {c
+void copy_sol_to_board(double *sol,int **result_arr, int N ) {
     int i, j, v;
     for (i = 0; i < N; i++) {
         for (j = 0; j < N; j++) {
